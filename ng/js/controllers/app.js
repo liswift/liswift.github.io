82f828a7a05cc3908564512901580f5f7095a163
liswift <greylight11@sina.com>
2016-11-28 16:43:15 +0800

    create my blog

--- ng/js/controllers/app.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by biQuan on 16-1-12.
- */
-define(['angular'],function(angular){
-    var testApp = angular.module('testApp',['ngRoute']);
-    return testApp;
-});
--- ng/js/controllers/qins-ctrl.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by biQuan on 16-1-12.
- */
-define(['jquery','angular','app'],function($,angular,testApp){
-    testApp.controller('QinsCtrl',['$scope',function($scope){
-
-    }]);
-});
